41c297e5b3b4ad04ea9867a704df31e384200ee6
moohameduddin704 <moohameduddin704@gmail.com>
2025-03-13 15:12:49 +0000

    Add 9.php

---
+++ src/9.php
@@ -0,0 +1,18 @@
+<?php
+// function to calculate the sum of two numbers
+function sum($a, $b) {
+return $a + $b;
+}
+// function to calculate the difference of two numbers
+function diff($a, $b) {
+return $a - $b;
+}
+// function to calculate the product of two numbers
+function prod($a, $b) {
+return $a * $b;
+}
+// function to calculate the quotient of two numbers
+function quot($a, $b) {
+return $a / $b;
+}
+?>
