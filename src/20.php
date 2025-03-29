ef95e70b87066738843f004c462217e7427908f3
moohameduddin704 <moohameduddin704@gmail.com>
2025-03-29 18:43:28 +0000

    Add 20.php

---
+++ src/20.php
@@ -0,0 +1,13 @@
+<?php
+// This is a simple PHP code to demonstrate some basic operations.
+// You can use this code as a starting point in your mathematics assignments.
+
+// Example of adding two numbers
+echo "Adding 5 and 10: " . (5 + 10) . "\n";
+
+// Example of subtracting two numbers
+echo "Subtracting 7 from 10: " . (10 - 7) . "\n";
+
+// Example of dividing two numbers
+echo "Dividing 20 by 4: " . (20 / 4) . "\n";
+?>
