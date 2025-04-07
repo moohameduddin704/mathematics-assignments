1f80dfbbef41e421242415ca8892179c03e8c55d
moohameduddin704 <moohameduddin704@gmail.com>
2025-04-07 14:29:59 +0000

    Add 27.php

---
+++ src/27.php
@@ -0,0 +1,11 @@
+<?php
+// Define variables and initialize arrays
+$var1 = "value1";
+$var2 = array("key1" => "value1", "key2" => "value2");
+
+// Print variable names
+echo "Variables: $var1, $var2\n";
+
+// Example usage of arrays in PHP
+print_r($var1);
+?>
