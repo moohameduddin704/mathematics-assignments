e8915afbc4483b21a466e8b4bc8d1302fa8430b6
moohameduddin704 <moohameduddin704@gmail.com>
2025-04-23 11:17:09 +0000

    Add 31.php

---
+++ src/31.php
@@ -0,0 +1,8 @@
+<?php
+$assignment_name = "Mathematics Assignment 1";
+$student_id = "ABC123";
+$subject_code = "Mathematics 101";
+
+// Code to be executed for Mathematics Assignment 1
+echo "Hello, $student_id! Your current subject code is: $subject_code.";
+?>
