67a8f88203c0001f79ee4429996168a10bd32016
moohameduddin704 <moohameduddin704@gmail.com>
2025-05-19 20:57:55 +0000

    Add 39.php

---
+++ src/39.php
@@ -0,0 +1,11 @@
+<?php
+// Define variables and constants
+$var1 = "value1";
+$var2 = "value2";
+
+// Perform some operation
+$result = $var1 + $var2;
+
+// Print the result
+echo "The result of $var1 plus $var2 is: $result";
+?>
