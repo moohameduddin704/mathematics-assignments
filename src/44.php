25a44f4c6368064905d3fe8624d5f6aa114ff97f
moohameduddin704 <moohameduddin704@gmail.com>
2025-05-26 15:40:51 +0000

    Add 44.php

---
+++ src/44.php
@@ -0,0 +1,11 @@
+<?php
+// Define variables and constants from math assignment
+$x = 5;
+$y = 3.2;
+
+// Calculate x^2 + y * 7
+$result = pow($x, 2) + $y * 7;
+
+// Print the result
+echo "Result: " . $result;
+?>
